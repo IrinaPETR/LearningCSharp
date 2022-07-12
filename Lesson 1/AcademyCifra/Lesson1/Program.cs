d5428eb336fadfc4b7690dd179e07f1a9741df40
IrinaP <irpetrovamex@gmail.com>
2022-07-13 02:48:57 +0400

    Create Program.cs Lesson1

---
+++ Lesson 1/AcademyCifra/Lesson1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Lesson1
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Как Вас зовут?");
+            string? name = Console.ReadLine();
+            Console.WriteLine($"Доброго времени суток \n {name}");
+            Console.ReadKey();
+        }
+    }
+
+}
